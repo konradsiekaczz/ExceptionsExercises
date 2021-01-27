3d7d181a88ca1009e55288bf64fb99e79ce6677d
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-27 17:52:18 +0100

    using try catch block to handle exceptions

--- src/main/java/StackTraceExample.java
+++ src/main/java/StackTraceExample.java
@@ -1,7 +1,15 @@
 public class StackTraceExample {
     public static void main(String[] args) {
         StackTraceExample stackTraceExample = new StackTraceExample();
-        stackTraceExample.method1();
+//        stackTraceExample.method1();
+        int numOfSeconds = 0;
+        try {
+
+            stackTraceExample.getNumberOfSeconds(-4);
+        }catch (IllegalArgumentException e){
+            numOfSeconds = stackTraceExample.getNumberOfSeconds(-4 * -1);
+        }
+        System.out.println(numOfSeconds);
     }
 
     public void method1(){
@@ -16,4 +24,10 @@ public class StackTraceExample {
         throw new RuntimeException("Runtime Exceptions!");
     }
 
+    public int getNumberOfSeconds(int hour){
+        if (hour<0){
+            throw new IllegalArgumentException("Hour must be >= 0: " + hour);
+        }
+        return hour * 60 * 60;
+    }
 }

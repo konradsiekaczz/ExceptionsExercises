7cfce231aae0ae09bc2abb5f437bc9224661f719
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-27 18:09:22 +0100

    refactoring code

--- src/main/java/StackTraceExample.java
+++ src/main/java/StackTraceExample.java
@@ -6,26 +6,26 @@ public class StackTraceExample {
         try {
 
             stackTraceExample.getNumberOfSeconds(-4);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             numOfSeconds = stackTraceExample.getNumberOfSeconds(-4 * -1);
         }
         System.out.println(numOfSeconds);
     }
 
-    public void method1(){
+    public void method1() {
         method2();
     }
 
-    public void method2(){
+    public void method2() {
         method3();
     }
 
-    public void method3(){
+    public void method3() {
         throw new RuntimeException("Runtime Exceptions!");
     }
 
-    public int getNumberOfSeconds(int hour){
-        if (hour<0){
+    public int getNumberOfSeconds(int hour) {
+        if (hour < 0) {
             throw new IllegalArgumentException("Hour must be >= 0: " + hour);
         }
         return hour * 60 * 60;

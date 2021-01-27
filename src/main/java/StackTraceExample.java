236f1b70d7e37f18710f493a1ebdce391230ef00
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-27 17:41:26 +0100

    creating method which throw RuntimeExceptions

--- src/main/java/StackTraceExample.java
+++ src/main/java/StackTraceExample.java
@@ -1,3 +1,19 @@
 public class StackTraceExample {
+    public static void main(String[] args) {
+        StackTraceExample stackTraceExample = new StackTraceExample();
+        stackTraceExample.method1();
+    }
+
+    public void method1(){
+        method2();
+    }
+
+    public void method2(){
+        method3();
+    }
+
+    public void method3(){
+        throw new RuntimeException("Runtime Exceptions!");
+    }
 
 }

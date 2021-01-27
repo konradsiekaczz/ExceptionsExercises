e5fe661fc2ec183f16ae5712da71a9278d2f78e2
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-27 18:55:45 +0100

    creating method for calculating square root of number with handle exceptions

--- src/main/java/SquareRootExercise.java
+++ src/main/java/SquareRootExercise.java
@@ -1,2 +1,29 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SquareRootExercise {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calculateSqrtOfNumber();
+    }
+
+    public static void calculateSqrtOfNumber() {
+        System.out.println("Please provide number: ");
+        double userInput;
+        while (true) {
+            try {
+                userInput = sc.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong type of argument. Please try again.");
+                sc.next();
+            }
+        }
+        if (userInput < 0) {
+            throw new IllegalArgumentException("Number must be >= 0.");
+        }
+        System.out.println(String.format("Square root of %.4f is %.4f ", userInput, Math.sqrt(userInput)));
+    }
 }

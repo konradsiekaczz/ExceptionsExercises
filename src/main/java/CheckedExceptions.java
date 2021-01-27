488aa82f648b01358098bbdf5adf2c66ffdbe76c
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-27 18:22:48 +0100

    adding method with throws signature and finally block

--- src/main/java/CheckedExceptions.java
+++ src/main/java/CheckedExceptions.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+
 public class CheckedExceptions {
+    public static void main(String[] args) {
+        CheckedExceptions checkedExceptions = new CheckedExceptions();
+        try {
+            checkedExceptions.methodWithCheckedException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("Finally is always execute.");
+        }
+    }
+
+    private void methodWithCheckedException() throws IOException {
+        throw new IOException();
+    }
 }

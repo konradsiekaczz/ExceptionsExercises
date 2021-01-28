8f7c7a6cfe2ff2874eea7282bddd396b6d51dc9b
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-28 11:26:21 +0100

    adding class Main to test exceptions

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Nurse jenny = new Nurse("Jenny", 25);
+
+        try {
+            jenny.doSomething();
+        }catch (MyException e){
+            e.printStackTrace();
+            System.out.println("Catch my exceptions.");
+        }
+    }
+}
